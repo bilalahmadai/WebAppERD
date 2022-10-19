567a3fee92c7d10004f76ab109769dcf90c9f9d6
bilalansar3 <bilalahmadai971@gmail.com>
2022-10-19 19:37:16 +0500

    creating MODEL (Book.cs) and (data>>.json)

--- WebAppERD/Model/Book.cs
+++ WebAppERD/Model/Book.cs
@@ -1,6 +1,35 @@
-﻿namespace WebAppERD.Model
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebAppERD.Model
 {
+   // this class will be use as to store  string data in INTO Json formate in json.file
     public class Book
     {
+
+        //this is used for to get into Jsonfile attribues of entitiy
+        //by defualt it gets same as in Json file but if want to use Book_id instead of id 
+        //then we ude JsonPropertyName... concept
+        [JsonPropertyName("id")]
+        public int book_id
+        {
+            get; set;
+        }
+        [JsonPropertyName("name")]
+        public int name
+        {
+            get; set;
+        }
+        [JsonPropertyName("image")]
+        public int image
+        {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Book>(this);
+        }
+
     }
 }
