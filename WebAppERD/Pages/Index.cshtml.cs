493c95db177b27854ceffdec09e5f4ee712ec24e
bilalansar3 <bilalahmadai971@gmail.com>
2022-10-19 20:04:19 +0500

    Create var for services & book record index file updated

--- WebAppERD/Pages/Index.cshtml.cs
+++ WebAppERD/Pages/Index.cshtml.cs
@@ -5,16 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAppERD.Model;
+using WebAppERD.Services;
 
 namespace WebAppERD.Pages
 {
     public class IndexModel : PageModel
     {
+        //VAR FOR SERVICE 
+        JsonFileBookService BookService;
+        // var for book records
+
+        public IEnumerable<Book> Books;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        // added , JsonFileBookService services
+        public IndexModel(ILogger<IndexModel> logger,
+           JsonFileBookService services)
         {
             _logger = logger;
+            //added
+            BookService = services;
         }
 
         public void OnGet()
